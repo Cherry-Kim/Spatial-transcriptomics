afb431d88ce9da3e72c611097ff96a7ad8f39e4e
Cherry-Kim <64776690+Cherry-Kim@users.noreply.github.com>
2024-10-17 10:43:15 -0700

    Update FeaturePlot_xenium.R

--- FeaturePlot_xenium.R
+++ FeaturePlot_xenium.R
@@ -19,3 +19,12 @@
         FeaturePlot(object_sub2, features =features, ncol =1,keep.scale="all")
         FeaturePlot(object_sub3, features =features, ncol =1,keep.scale="all")
 
+
+features = c("ACTA2","MRC1","CD68","CX3CR1")
+
+        p <- ImageDimPlot(xenium.obj, fov = "fov", molecules = features, nmols = 20000)+ theme( legend.text = element_text(size = 20),  legend.title = element_text(size = 18) )
+        p + scale_fill_manual(values = c("gray"))
+
+        ImageFeaturePlot(xenium.obj, features = features, max.cutoff = c(25, 35, 12, 10), size = 0.75, cols = c("white", "red"))
+
+        ImageDimPlot(xenium.obj, fov = "zoom", axes = TRUE, border.color = "white", border.size = 0.1, cols = "polychrome", coord.fixed = FALSE, molecules = features, nmols = 20000) + theme( legend.text = element_text(size = 20),  legend.title = element_blank(), axis.text = element_text(size = 15))
